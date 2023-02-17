35188d3d6ca4e8ac9ae2fdadd9802140f6200140
ZorkDaNerd <zork9898@colostate.edu>
2023-02-16 18:58:46 -0700

    Movement and stuff

--- Unity-VR-ADHD/Assets/AnimateHandOnInput.cs
+++ Unity-VR-ADHD/Assets/AnimateHandOnInput.cs
@@ -20,7 +20,7 @@ public class AnimateHandOnInput : MonoBehaviour
     {
         float triggerValue = pinchAnimationAction.action.ReadValue<float>();   
         handAnimator.SetFloat("Trigger", triggerValue);
-        //Debug.Log(triggerValue);
+
         float gripValue = gripAnimationAction.action.ReadValue<float>();
         handAnimator.SetFloat("Grip", gripValue);
     }
