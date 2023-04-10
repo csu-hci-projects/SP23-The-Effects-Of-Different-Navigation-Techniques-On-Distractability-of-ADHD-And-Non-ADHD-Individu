48704a39bb7a931d2a5c11b5b5ed3eea33283350
ZorkDaNerd <zork9898@colostate.edu>
2023-04-09 22:14:44 -0600

    Fixed the jagged line

--- Unity-VR-ADHD/Assets/Scripts/Followline.cs
+++ Unity-VR-ADHD/Assets/Scripts/Followline.cs
@@ -5,55 +5,52 @@ using UnityEngine.AI;
 
 public class Followline : MonoBehaviour
 {
-//Make a unity script that takes a navemesh agent and a line render and renders the navmesh line
-//Make it also folliw up and down the line
-
-
-
     public NavMeshAgent agent;
     public Transform goal;
     public LineRenderer line;
+    public float heightOffset = 0.5f;
 
-    // Start is called before the first frame update
     void Start()
     {
         line.positionCount = 0;
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent = GetComponent<NavMeshAgent>();
         agent.destination = goal.position; 
     }
 
-    // Update is called once per frame
     void Update()
-{
-    if(agent.hasPath)
     {
-        Vector3[] smoothedPath = SmoothPath(agent.path.corners);
-        line.positionCount = smoothedPath.Length;
-        line.SetPositions(smoothedPath);
+        if (agent.hasPath)
+        {
+            Vector3[] smoothedPath = SmoothPath(agent.path.corners);
+            line.positionCount = smoothedPath.Length;
+            for (int i = 0; i < smoothedPath.Length; i++)
+            {
+                line.SetPosition(i, smoothedPath[i] + Vector3.up * heightOffset);
+            }
+        }
+        else
+        {
+            line.positionCount = 0;
+        }
     }
-    else
-    {
-        line.positionCount = 0;
-    }
-}
 
-Vector3[] SmoothPath(Vector3[] path)
+    Vector3[] SmoothPath(Vector3[] path)
 {
     List<Vector3> smoothedPath = new List<Vector3>();
     smoothedPath.Add(path[0]);
     for (int i = 1; i < path.Length; i++)
     {
-        Vector3 direction = (path[i] - path[i-1]).normalized;
-        float distance = Vector3.Distance(path[i-1], path[i]);
-        int steps = Mathf.CeilToInt(distance / 0.1f); // Increase or decrease the step size to adjust smoothing
+        Vector3 direction = (path[i] - path[i - 1]).normalized;
+        float distance = Vector3.Distance(path[i - 1], path[i]);
+        int steps = Mathf.CeilToInt(distance / 0.1f);
         float stepSize = distance / steps;
         for (int j = 1; j < steps; j++)
         {
-            Vector3 point = path[i-1] + (direction * j * stepSize);
-            RaycastHit hit;
-            if (Physics.Raycast(point + Vector3.up * 10f, Vector3.down, out hit, 20f, NavMesh.AllAreas))
+            Vector3 point = path[i - 1] + (direction * j * stepSize);
+            NavMeshHit hit;
+            if (NavMesh.SamplePosition(point, out hit, 0.1f, NavMesh.AllAreas))
             {
-                point = hit.point + Vector3.up * 0.1f; // Increase or decrease the height offset to adjust line position
+                point = hit.position + Vector3.up * 0.1f;
             }
             smoothedPath.Add(point);
         }
